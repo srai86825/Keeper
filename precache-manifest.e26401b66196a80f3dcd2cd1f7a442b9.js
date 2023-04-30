315c65778f9b3dba0fa2f9268decaf924217b63f
saurabh kumar rai <srai86825@gmail.com>
2023-04-30 17:59:01 +0530

    Updates

---
+++ precache-manifest.e26401b66196a80f3dcd2cd1f7a442b9.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce5c3557e94a2e509d1651a3e82a476f",
+    "url": "/Keeper/index.html"
+  },
+  {
+    "revision": "a4d931c63f47251f68ff",
+    "url": "/Keeper/static/js/2.260104d4.chunk.js"
+  },
+  {
+    "revision": "a4ea14031be0c9b4e19c",
+    "url": "/Keeper/static/js/main.5199f8b1.chunk.js"
+  },
+  {
+    "revision": "4b772ed70d71f9d0beff",
+    "url": "/Keeper/static/js/runtime-main.fd6e374a.js"
+  }
+]);
